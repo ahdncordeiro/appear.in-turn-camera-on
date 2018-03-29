e863cff5add16970315c973855be757949d64294
André Cordeiro <ahdncordeiro@gmail.com>
2018-03-29 12:16:43 -0300

    Adding logs

--- turn-camera-on.js
+++ turn-camera-on.js
@@ -1,6 +1,10 @@
 setInterval(function(){
+  Console.log('Verifying if the camera is off');
   var jQueryButton = $("button:contains('Turn camera back on')");
   if (jQueryButton.length > 0) {
+    Console.log('Camera OFF, turning camera ON.');
     jQueryButton.click();
+  } else {
+    Console.log('Camera still ON, nothing to do.');
   }
 }, 60000);
